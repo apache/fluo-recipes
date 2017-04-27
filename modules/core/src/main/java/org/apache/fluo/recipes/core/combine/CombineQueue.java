e8e6fdea7936356dd50530b8364b4156bfaaeaff
Keith Turner <kturner@apache.org>
2017-04-27 19:22:54 -0400

    WIP

--- modules/core/src/main/java/org/apache/fluo/recipes/core/combine/CombineQueue.java
+++ modules/core/src/main/java/org/apache/fluo/recipes/core/combine/CombineQueue.java
@@ -1,13 +1,21 @@
 package org.apache.fluo.recipes.core.combine;
 
+import java.util.Map;
+
+import org.apache.fluo.api.client.TransactionBase;
 import org.apache.fluo.api.config.FluoConfiguration;
+import org.apache.fluo.api.config.SimpleConfiguration;
 import org.apache.fluo.api.observer.ObserverProvider.Registry;
-import org.apache.fluo.recipes.core.map.CollisionFreeMap.Options;
 
 /**
  * @since 1.1.0
  */
 public class CombineQueue<K,V> {
+  
+ public void add(TransactionBase tx, Map<K, V> updates) {
+    
+  }
+  
   public void registerObserver(Registry obsRegistry, Combiner<K,V> combiner, ValueObserver<K, V> updateObserver){
     
   }
@@ -31,7 +39,9 @@ public class CombineQueue<K,V> {
   }
   
   
-  public static void main(String[] args) {
-    CombineQueue.configure(null).types(null, null).buckets(13).bucketsPerTablet(5).finish(null);
+  public static <K2,V2> CombineQueue<K2,V2> getInstance(String id, SimpleConfiguration appConfig) {
+    return null;
   }
+  
+ 
 }
